6620e952bdd7c6af8fefd9200ac1b1da9de6896e
Christina Dacanay <tina.dacanay@gmail.com>
2020-09-06 13:35:41 -0400

    Basically done.

--- main.js
+++ main.js
@@ -5,33 +5,57 @@ function init() {
   let pen = document.getElementById("pen");
   let content = document.getElementById("videoInsert");
   let tv = document.getElementById("main");
-
-  // playlist();
-
+  let frame = document.getElementById("frame-img");
+  let tag = null;
 
   //BUTTON CONTROLS
   headphone.addEventListener("click", () => {
     resetVid();
+    tag = 1;
+    frameImg("assets/fr-headphone.svg")
     videoPlay("assets/cow.mp4");
   });
 
   control.addEventListener("click", () => {
     console.log("click!");
     resetVid();
+    tag = 2;
+    frameImg("assets/fr-ctrl.svg")
     videoPlay("assets/loz.mp4");
   });
 
   book.addEventListener("click", () => {
     console.log("click!");
+    tag = 3;
+    frameImg("assets/fr-book.svg")
     playlist();
   });
 
   pen.addEventListener("click", () => {
     console.log("click!");
+    tag = 4;
+    frameImg("assets/fr-pen.svg")
     aboutPage();
   });
 
   //FUNCTIONS
+  function frameImg(source) {
+    console.log(tag);
+    frame.src = source;
+    frame.addEventListener("click", () => {
+      console.log('clicked');
+      if (tag == 1) {
+        window.open("https://open.spotify.com/user/1216455498?si=dJzREaqrTiy0m2lyXEpoHQ", "_blank");
+      } else if (tag == 2) {
+        console.log('vidogames');
+      } else if (tag == 3) {
+        window.open("https://www.goodreads.com/user/show/10366015-christina", "_blank");
+      } else {
+        window.open("http://cdacanay.com/", "_blank");
+      }
+    })
+
+  }
   function resetVid() {
     tv.innerHTML = `        
       <video id="videoInsert" autoplay loop>
@@ -56,7 +80,7 @@ function init() {
   function aboutPage() {
     tv.innerHTML = `<div id="about"><h1>live web assignment 1 <br>self-portrait</h1><br>
     <p>this is a website by <B>christina dacanay</b><br><br>
-      she's a illustrator, ux/ui designer, and burgeoning web developer.<br><br>
+      she's a illustrator, ux/ui designer, and web developer.<br><br>
       find more about this project on her blog and github.<br><br>
       click on the frame for more specific presences.</div>`;
   }
